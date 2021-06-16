0bd57e9e183496bb2d516621f23dfeff9f74528b
root <root@DESKTOP-O9RAJCO.localdomain>
2021-06-16 14:30:42 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Sumswap Info
+
+[![code style: prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg?style=flat-square)](https://github.com/prettier/prettier)
+
+Analytics site for the [Sumswap Protocol](https://sumswap.org).
+
+### To Start Development
+
+###### Installing dependencies
+```bash
+yarn
+```
+
+###### Running locally
+```bash
+yarn start
+```
---
+++ src/components/Attribution/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Attribution = () => (
+  <p className="attribution">
+    <a href="https://github.com/sumswap/sumswap-info" rel="noopener noreferrer" target="_blank">
+      Github
+    </a>{' '}
+    |{' '}
+    <a href="https://sumswap.org" rel="noopener noreferrer" target="_blank">
+      Sumswap
+    </a>{' '}
+    |{' '}
+    <a href="https://gifer.com/en/1Atv" rel="noopener noreferrer" target="_blank">
+      GIF
+    </a>
+  </p>
+)
+
+export default Attribution
